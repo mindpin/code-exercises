18527954d5a09955d16339b27257693fca2aba71
fushang318 <fushang318@gmail.com>
2015-10-26 12:58:23 +0800

    fix 2

--- 2/test_rspec.rb
+++ 2/test_rspec.rb
@@ -3,30 +3,30 @@ require File.expand_path("../test", __FILE__)
 RSpec.describe "test 2" do
 
   it "应缴税所得额是 1000" do
-    expect(run(1000).to eq(30)
+    expect(run(1000)).to eq(30)
   end
 
   it "应缴税所得额是 3000" do
-    expect(run(3000).to eq(295)
+    expect(run(3000)).to eq(295)
   end
 
   it "应缴税所得额是 5000" do
-    expect(run(5000).to eq(445)
+    expect(run(5000)).to eq(445)
   end
 
   it "应缴税所得额是 20 000" do
-    expect(run(20000).to eq(3995)
+    expect(run(20000)).to eq(3995)
   end
 
   it "应缴税所得额是 40 000" do
-    expect(run(40000).to eq(9245)
+    expect(run(40000)).to eq(9245)
   end
 
   it "应缴税所得额是 60 000" do
-    expect(run(60000).to eq(15495)
+    expect(run(60000)).to eq(15495)
   end
 
   it "应缴税所得额是 90 000" do
-    expect(run(90000).to eq(26995)
+    expect(run(90000)).to eq(26995)
   end
 end
